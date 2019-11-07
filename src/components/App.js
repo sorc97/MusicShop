1d7fa43e2a654beb8a7dfcdd9cbf4bf383350dde
sorc97 <nnd64809200@gmail.com>
2019-11-07 20:39:20 +0300

    Webpack config and initial project structuring

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './stylesheets/App.css'
+
+const App = () =>
+  <div className='wrapper'>
+
+  </div>
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    publicPath: '/build'
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 4000,
+    contentBase: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
