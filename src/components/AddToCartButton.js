006ff466f4af9c62a4e5786759d5d53907df6c6d
sorc97 <nnd64809200@gmail.com>
2019-11-20 18:10:28 +0300

    Single adding product to cart from ProductsList

--- src/components/AddToCartButton.js
+++ src/components/AddToCartButton.js
@@ -4,16 +4,8 @@ import './stylesheets/AddToCartButton.css'
 const AddToCartButton = ({
   addProductToCart, isInCart, id, removeProduct, additionStyle
 }) => {
-
+  
   return(
-    /* <button
-      className={(!isInCart) ? 'product-button': 'product-button inCart'}
-      onClick={() => addProductToCart(id)}
-      style={additionStyle}
-    >
-      В корзину {isInCart && `(${isInCart.amount})` }
-    </button> */
-
     (isInCart) ?
       <button
         className="product-button remove"
